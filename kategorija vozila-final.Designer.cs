52af9c7b1a927d9b8cfa777a9ec8110930098fba
AntonioDusti1 <152974953+AntonioDusti1@users.noreply.github.com>
2024-10-03 09:53:28 +0200

    Add files via upload

---
+++ kategorija vozila-final.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace kategorijavozlia3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtModel = new System.Windows.Forms.TextBox();
+            this.txtGodinaProizvodnje = new System.Windows.Forms.TextBox();
+            this.txtBrojKotača = new System.Windows.Forms.TextBox();
+            this.txtIspis = new System.Windows.Forms.TextBox();
+            this.btnUnesi = new System.Windows.Forms.Button();
+            this.btnObradi = new System.Windows.Forms.Button();
+            this.btnIspis = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtModel
+            // 
+            this.txtModel.Location = new System.Drawing.Point(120, 79);
+            this.txtModel.Multiline = true;
+            this.txtModel.Name = "txtModel";
+            this.txtModel.Size = new System.Drawing.Size(163, 26);
+            this.txtModel.TabIndex = 0;
+            // 
+            // txtGodinaProizvodnje
+            // 
+            this.txtGodinaProizvodnje.Location = new System.Drawing.Point(120, 159);
+            this.txtGodinaProizvodnje.Multiline = true;
+            this.txtGodinaProizvodnje.Name = "txtGodinaProizvodnje";
+            this.txtGodinaProizvodnje.Size = new System.Drawing.Size(163, 26);
+            this.txtGodinaProizvodnje.TabIndex = 1;
+            // 
+            // txtBrojKotača
+            // 
+            this.txtBrojKotača.Location = new System.Drawing.Point(120, 244);
+            this.txtBrojKotača.Multiline = true;
+            this.txtBrojKotača.Name = "txtBrojKotača";
+            this.txtBrojKotača.Size = new System.Drawing.Size(163, 26);
+            this.txtBrojKotača.TabIndex = 2;
+            // 
+            // txtIspis
+            // 
+            this.txtIspis.Location = new System.Drawing.Point(430, 79);
+            this.txtIspis.Multiline = true;
+            this.txtIspis.Name = "txtIspis";
+            this.txtIspis.Size = new System.Drawing.Size(308, 198);
+            this.txtIspis.TabIndex = 3;
+            // 
+            // btnUnesi
+            // 
+            this.btnUnesi.Location = new System.Drawing.Point(147, 324);
+            this.btnUnesi.Name = "btnUnesi";
+            this.btnUnesi.Size = new System.Drawing.Size(85, 38);
+            this.btnUnesi.TabIndex = 4;
+            this.btnUnesi.Text = "Unesi";
+            this.btnUnesi.UseVisualStyleBackColor = true;
+            this.btnUnesi.Click += new System.EventHandler(this.btnUnesi_Click);
+            // 
+            // btnObradi
+            // 
+            this.btnObradi.Location = new System.Drawing.Point(329, 324);
+            this.btnObradi.Name = "btnObradi";
+            this.btnObradi.Size = new System.Drawing.Size(90, 38);
+            this.btnObradi.TabIndex = 5;
+            this.btnObradi.Text = "Obradi";
+            this.btnObradi.UseVisualStyleBackColor = true;
+            this.btnObradi.Click += new System.EventHandler(this.btnObradi_Click);
+            // 
+            // btnIspis
+            // 
+            this.btnIspis.Location = new System.Drawing.Point(507, 324);
+            this.btnIspis.Name = "btnIspis";
+            this.btnIspis.Size = new System.Drawing.Size(79, 38);
+            this.btnIspis.TabIndex = 6;
+            this.btnIspis.Text = "Ispis";
+            this.btnIspis.UseVisualStyleBackColor = true;
+            this.btnIspis.Click += new System.EventHandler(this.btnIspis_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(120, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Model";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(116, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Godina Proizvodnje";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(116, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Broj_Kotača";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnIspis);
+            this.Controls.Add(this.btnObradi);
+            this.Controls.Add(this.btnUnesi);
+            this.Controls.Add(this.txtIspis);
+            this.Controls.Add(this.txtBrojKotača);
+            this.Controls.Add(this.txtGodinaProizvodnje);
+            this.Controls.Add(this.txtModel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtModel;
+        private System.Windows.Forms.TextBox txtGodinaProizvodnje;
+        private System.Windows.Forms.TextBox txtBrojKotača;
+        private System.Windows.Forms.TextBox txtIspis;
+        private System.Windows.Forms.Button btnUnesi;
+        private System.Windows.Forms.Button btnObradi;
+        private System.Windows.Forms.Button btnIspis;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ kategorija vozila-final.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kategorijavozlia3
+{
+    public partial class Form1 : Form
+    {
+        private int brojMotocikala= 0;
+        private int brojAutomobila = 0;
+        private int brojKamiona = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUnesi_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtGodinaProizvodnje.Text, out int godina) || godina < 1886 || godina > DateTime.Now.Year)
+            {
+                MessageBox.Show("Unesite valjanu godinu proizvodnje.");
+                return;
+            }
+            if (!int.TryParse(txtBrojKotača.Text, out int brojKotaca) || brojKotaca < 2 || brojKotaca % 2 != 0)
+            {
+                MessageBox.Show("Unesite validan broj kotača (mora biti paran i veći od 2).");
+                return;
+            }
+            string model = txtModel.Text;
+            if (brojKotaca == 2)
+            {
+                brojMotocikala++;
+                txtIspis.AppendText($"Dodano vozilo: {model} - " +
+                    $"Motocikl\r\n");
+            }
+            else if (brojKotaca == 4)
+            {
+                brojAutomobila++;
+                txtIspis.AppendText($"Dodano vozilo: {model} - " +
+                    $"Automobil\r\n");
+            }
+            else
+            {
+                brojKamiona++;
+                txtIspis.AppendText($"Dodano vozilo: {model} - " +
+                    $"Kamion\r\n");
+            }
+
+            txtModel.Clear();
+            txtGodinaProizvodnje.Clear();
+            txtBrojKotača.Clear();
+        }
+
+        private void btnObradi_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Podaci su primljeni.");
+        }
+
+        private void btnIspis_Click(object sender, EventArgs e)
+        {
+            txtIspis.AppendText($"\nUkupan broj vozila:\r\n");
+            txtIspis.AppendText($"Motocikli: {brojMotocikala}\r\n");
+            txtIspis.AppendText($"Automobili: {brojAutomobila}\r\n");
+            txtIspis.AppendText($"Kamioni: {brojKamiona}\r\n");
+        }
+    }
+}
